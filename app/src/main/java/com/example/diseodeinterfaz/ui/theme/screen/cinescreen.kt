18d80c63232d97e01ba3f0f0d0686fe67723222c
DamianGaelJR <gaeljimenez463@gmail.com>
2024-09-17 08:52:59 -0600

    Initial commit

---
+++ app/src/main/java/com/example/diseodeinterfaz/ui/theme/screen/cinescreen.kt
@@ -0,0 +1,10 @@
+package com.example.diseodeinterfaz.ui.theme.screen
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+@Composable
+fun cinescreen(){
+    Scaffold {
+
+    }
+}
